633dddf05e6ca05db033fab1ff76621d1f8904d2
Conduent <wasim.bari@conduent.com>
2021-04-07 01:50:02 +0530

    modified code

--- CounterContract/UI/server.js
+++ CounterContract/UI/server.js
@@ -2,9 +2,11 @@ var express = require('express');
 var app = express();
 var path = require('path');
 const Web3 = require('web3')
-
+//for the alert box
+var alert = require('alert')
 //mongodb
 const {MongoClient} = require('mongodb');
+//connection string for the mongoDB
 const uri = "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false";
 const client = new MongoClient(uri);
 
@@ -25,7 +27,7 @@ app.post('/increment', function(req, res){
 
 	var curVal = currentVal();
 	curVal.then(counterVal => {
-		console.log(counterVal); // fetched movies
+		console.log(counterVal); 
 		res.end(counterVal)
 	  });
     
@@ -35,7 +37,7 @@ app.post('/increment', function(req, res){
 
 	var resVal = resetVal();
 	resVal.then(reset => {
-		console.log("value has been reset."); // fetched movies
+		console.log("value has been reset.");
 		res.end("value has been reset")
 	  });
     
@@ -45,37 +47,15 @@ app.listen(8081);
 console.log("server is up");
 
 
-// async function  connectToMongo() {
-// 	try {
-// 		// Connect to the MongoDB cluster
-// 		var conn = await client.connect();
-// 		console.log("connected");
-
-// 		return conn;
-
-// 	} catch (e) {
-// 		console.error(e);
-// 	} finally {
-// 		await client.close();
-// 	}
-// }
-
-
 async function insertValue(incrVal, taskType){
 
 	try{
-
 		var con = await client.connect();
 		var today = new Date();
 		var time = today.getFullYear() + "-" + today.getMonth()+1 + "-" + today.getDate() + today.getFullYear() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
 		
-
 		if(taskType == "increment") {
 			console.log("increment Value");
-			// var con = await client.connect();
-			// var today = new Date();
-			// var time = today.getFullYear() + "-" + today.getMonth()+1 + "-" + today.getDate() + today.getFullYear() + " " + today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-		
 			var myobj = { incVal: incrVal, timestamp: time, comment:"Incremented value"};
 			let result = await con.db("Counter").collection("countercoll").insertOne(myobj);
 		
@@ -83,6 +63,7 @@ async function insertValue(incrVal, taskType){
 			var myobj = { incVal: incrVal, timestamp: time, comment:"Value has been reset to zero."};
 			let result = await con.db("Counter").collection("countercoll").insertOne(myobj);
 		}
+
 	}catch (e) {
 	
 		console.error(e);
@@ -96,11 +77,11 @@ async function insertValue(incrVal, taskType){
 // ganache url
 const web3 = new Web3("http://127.0.0.1:7545")
 
-const address = '0x181f92EeEabB05Dc04F0C2cbD70753f9ddCa576A' // Your account address goes here
-web3.eth.getBalance(address, (err, wei) => { balance = web3.utils.fromWei(wei, 'ether') 
-				console.log("balance");
-				console.log(balance);
-})
+const address = '0x181f92EeEabB05Dc04F0C2cbD70753f9ddCa576A' 
+// web3.eth.getBalance(address, (err, wei) => { balance = web3.utils.fromWei(wei, 'ether') 
+// 				console.log("balance");
+// 				console.log(balance);
+// })
 
 const abi = [
 	{
@@ -187,7 +168,7 @@ const abi = [
 	}
 ]
 	
-const contAddr = '0x485b511deF0EEF363bC422B4dA5df0B16BADb372' //'0x69ff9805bD7B3EC51dc01Dc94dfcd57390b6Cb06'
+const contAddr = '0xc717092d19E7B4D7035809216f57837bb7ED38b3' //'0x69ff9805bD7B3EC51dc01Dc94dfcd57390b6Cb06'
 const contract = new web3.eth.Contract(abi, contAddr)
 
 async function sendT() {
@@ -196,13 +177,19 @@ async function sendT() {
 	console.log("incremented value is");
 	console.log(inc.events["incrEvent"]["returnValues"]["_value"]); //worked
     var incrVal= inc.events["incrEvent"]["returnValues"]["_value"];
-    
+
 
 	if(incrVal == 10) {
+		//as the new value is 10 , so will be reset to 0 but this new value will be saved in Db too
+		insertValue(incrVal, "increment");
+	
+		alert("incrVal's  previous value was 9 so, new value is " + incrVal+ " .Reseting the value to 0")
 		console.log("Calling reset")
 		var resetVal = await contract.methods.reset().send({from:address});
+		//reseting the value and logging it to the DB.
 		insertValue(0,"reset");
 	}else {
+		//logging the new incremented value to the DB.
 		insertValue(incrVal, "increment");
 		console.log("Send completed");
 	}
@@ -213,10 +200,10 @@ async function sendT() {
 
 
 async function resetVal() {
-//	if(incrVal == 10) {
+
 		console.log("Calling reset")
 		var resetVal = await contract.methods.reset().send({from:address});
-//	}
+
 } 
 
 async function currentVal() {
